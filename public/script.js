be2dc074e7be4ed3cb9cce1e8696171748284679
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-10-29 03:44:22 -0400

    Call user within room and have them answer the call

--- public/script.js
+++ public/script.js
@@ -19,18 +19,30 @@ navigator.mediaDevices
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
   });
 
 peer.on("open", (id) => {
   socket.emit("join-room", ROOM_ID, id);
 });
 
-socket.on("user-connected", (userId) => {
-  connectToNewUser(userId);
-});
-
-const connectToNewUser = (userId) => {
-  console.log(userId);
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
 };
 
 const addVideoStream = (video, stream) => {
