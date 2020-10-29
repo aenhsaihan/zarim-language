eb6c81a930212bc732b45aee0c99644814f2b18f
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-10-28 22:35:52 -0400

    Allow another user to join the room and broadcast their entrance to other users already in the room

--- public/script.js
+++ public/script.js
@@ -15,7 +15,15 @@ navigator.mediaDevices
     addVideoStream(myVideo, stream);
   });
 
-socket.emit("join-room");
+socket.emit("join-room", ROOM_ID);
+
+socket.on("user-connected", () => {
+  connectToNewUser();
+});
+
+const connectToNewUser = () => {
+  console.log("new user");
+};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
