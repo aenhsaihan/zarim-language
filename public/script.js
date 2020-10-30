263780d8386f0fc1ac8c925892d10bdb54cef6e0
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-10-30 01:12:43 -0400

    Mute and unmute microphone

--- public/script.js
+++ public/script.js
@@ -70,3 +70,24 @@ $("html").keydown((e) => {
 socket.on("createMessage", (message) => {
   $(".messages").append(`<li class="message"><b>user</b><br/>${message}</i>`);
 });
+
+const setMuteButton = () => {
+  const html = `<i class="fas fa-microphone"></i><span>Mute</span>`;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `<i class="unmute fas fa-microphone-slash"></i><span>Unmute</span>`;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+};
