52ad0ec894bd9e46c3ccbe9d17520dd79c40d9df
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-11-06 02:48:34 -0500

    Remove unnecessary contract and configure dependencies to get compilation and testing working again

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var Zarim = artifacts.require("./Zarim.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(SimpleStorage);
   deployer.deploy(Zarim);
 };
