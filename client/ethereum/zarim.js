86465a5de01dec49b8815cbdef906810cd7e1c18
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-11-12 18:17:07 -0500

    Use an instance of the zarim contract for contract interaction

---
+++ client/ethereum/zarim.js
@@ -0,0 +1,32 @@
+import Zarim from "../ethereum/build/Zarim.json";
+import getWeb3 from "../ethereum/getWeb3";
+
+const retrieveInstance = async () => {
+  try {
+    // Get network provider and web3 instance.
+    const web3 = await getWeb3();
+
+    // Use web3 to get the user's accounts.
+    const accounts = await web3.eth.getAccounts();
+
+    // Get the contract instance.
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = Zarim.networks[networkId];
+    const instance = new web3.eth.Contract(
+      Zarim.abi,
+      deployedNetwork && deployedNetwork.address
+    );
+
+    return instance;
+  } catch (error) {
+    // Catch any errors for any of the above operations.
+    alert(
+      `Failed to load web3, accounts, or contract. Check console for details.`
+    );
+    console.error(error);
+  }
+};
+
+const instance = retrieveInstance();
+
+export default instance;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,65 +1,21 @@
 import React, { Component } from "react";
-import Zarim from "../ethereum/build/Zarim.json";
-import getWeb3 from "../ethereum/getWeb3";
+import zarim from "../ethereum/zarim";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  static async getInitialProps() {
+    const contract = await zarim;
+    const speakersCount = await contract.methods.getSpeakersCount(0).call();
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = Zarim.networks[networkId];
-      const instance = new web3.eth.Contract(
-        Zarim.abi,
-        deployedNetwork && deployedNetwork.address
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
-      );
-      console.error(error);
-    }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.getSpeakersCount(0).call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+    return { speakersCount, contract };
+  }
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
     return (
       <div className="App">
         <h1>Good to Go!!!</h1>
         <p>Your Truffle Box is installed and ready.</p>
         <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <div>The stored value is: {this.props.speakersCount}</div>
       </div>
     );
   }
