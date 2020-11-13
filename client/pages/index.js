1c05a9c32de34a80fbc61e3428a5c3517c4e931d
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-11-13 01:24:15 -0500

    Render native speaker of language zero

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
 import zarim from "../ethereum/zarim";
 
 class App extends Component {
   static async getInitialProps() {
     const contract = await zarim;
     const speakersCount = await contract.methods.getSpeakersCount(0).call();
+    const nativeSpeaker = await contract.methods.nativeSpeakers(0, 0).call();
 
-    return { speakersCount, contract };
+    return { speakersCount, contract, nativeSpeaker };
+  }
+
+  renderNativeSpeakers() {
+    const item = {
+      header: this.props.nativeSpeaker,
+      description: <a>View speaker</a>,
+      fluid: true,
+    };
+    return <Card.Group items={[item]} />;
   }
 
   render() {
@@ -16,6 +27,7 @@ class App extends Component {
         <p>Your Truffle Box is installed and ready.</p>
         <h2>Smart Contract Example</h2>
         <div>The stored value is: {this.props.speakersCount}</div>
+        <div>{this.renderNativeSpeakers()}</div>
       </div>
     );
   }
