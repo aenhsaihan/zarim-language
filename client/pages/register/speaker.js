01fd253fc90f0c6ea70ecf19655c81b12396a9b9
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-11-18 02:13:21 -0500

    Capture values for all speaker attributes in event handler

--- client/pages/register/speaker.js
+++ client/pages/register/speaker.js
@@ -5,6 +5,9 @@ import Layout from "../../components/Layout";
 class RegisterSpeaker extends Component {
   state = {
     age: "",
+    gender: "",
+    country: "",
+    languages: "",
   };
 
   render() {
@@ -23,17 +26,32 @@ class RegisterSpeaker extends Component {
 
           <Form.Field>
             <label>Gender</label>
-            <input />
+            <input
+              value={this.state.gender}
+              onChange={(event) =>
+                this.setState({ gender: event.target.value })
+              }
+            />
           </Form.Field>
 
           <Form.Field>
             <label>Country</label>
-            <input />
+            <input
+              value={this.state.country}
+              onChange={(event) =>
+                this.setState({ country: event.target.value })
+              }
+            />
           </Form.Field>
 
           <Form.Field>
             <label>Native language(s)</label>
-            <input />
+            <input
+              value={this.state.languages}
+              onChange={(event) =>
+                this.setState({ languages: event.target.value })
+              }
+            />
           </Form.Field>
 
           <Button primary>Register</Button>
