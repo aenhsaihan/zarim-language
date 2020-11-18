2a4e470dc8a834f27326e27f5137bb5025dcea7c
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-11-18 01:42:59 -0500

    Constrain interface elements and add a bit of margin from the top

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,7 +3,7 @@ import { Menu } from "semantic-ui-react";
 
 export default () => {
   return (
-    <Menu>
+    <Menu style={{ marginTop: "10px" }}>
       <Menu.Item>Zarim</Menu.Item>
       <Menu.Menu position="right">
         <Menu.Item>Sessions</Menu.Item>
