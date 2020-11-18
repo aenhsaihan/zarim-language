840193f5cd73255a5427c25814d96ccb532ba132
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-11-18 01:50:15 -0500

    Add new route to register a speaker

---
+++ client/pages/register/speaker.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class RegisterSpeaker extends Component {
+  render() {
+    return <h1>Registration of new speaker</h1>;
+  }
+}
+
+export default RegisterSpeaker;
